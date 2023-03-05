83a9f5f3b47845179e235959b21cc26cca37c81d
Sakherr <sakhershteyat@gmail.com>
2023-03-05 10:48:17 +0000

    S

---  app.js
+++  app.js
@@ -34,3 +34,45 @@ if (!skip) {
 } else {
   alert("Ok, welcome!");
 }
+
+
+function askThreeRandomQuestions() {
+  const question1 = prompt("Do you like a video games? (yes or no)");
+  const question2 = prompt("DO you like ride a horse? (yes or no)");
+  const question3 = prompt("Do you enjoy watching comedy movies? (yes or no)");
+
+  const answers = [question1, question2, question3];
+
+  return answers;
+}
+
+function validateAnswer(answer) {
+  if (!answer || (answer.toLowerCase() !== "yes" && answer.toLowerCase() !== "no")) {
+    alert (invalid);
+  }
+
+  return answer;
+}
+
+
+function validateAnswersArray(answers) {
+  const validatedAnswers = [];
+  
+  for (let i = 0; i < answers.length; i++) {
+    const answer = validateAnswer(answers[i]);
+    validatedAnswers.push(answer);
+  }
+  
+  return validatedAnswers;
+}
+
+function printArrayItems(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+
+const questions = askThreeRandomQuestions();
+const validatedAnswers = validateAnswersArray(questions);
+printArrayItems(validatedAnswers);
